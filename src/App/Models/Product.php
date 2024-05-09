5c6e19bdab10e858ca36f5a88cc255055465b615
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-05-09 09:49:06 +0600

    initial commit

---
+++ src/App/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Models;
+
+use App\Database;
+use PDO;
+class Product
+{
+    public function __construct(private Database $database)
+    {
+        
+    }
+    
+    
+    public function getData():array
+    {
+      
+      
+        $pdo=$this->database->getConnection();
+        $stmt = $pdo->query("SELECT * FROM product");
+      return  $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    }
+}
